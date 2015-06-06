bb53108911dc5d3f250451e5d4f24824044d0ff9
清水美希 <miki@shimizumiki-no-MacBook-Air.local>
2015-06-06 15:59:42 +0900

    first commit

--- CountLiT/ViewController.swift
+++ CountLiT/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var number:Int = 0//初期化
+    @IBOutlet var label :UILabel!//@IBOutletは画面に表示するやつ、UILabelに属しているlabelという変数
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func tasu(){
+        number = number + 1;
+        label.text = String(number)//Int型だからStringにしなければlabel.textには入れることはできない。lavel.textはString型しか入れることはできない
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
